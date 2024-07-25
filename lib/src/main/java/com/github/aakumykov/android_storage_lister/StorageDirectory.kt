a549b7dd13ef8b7be31f8f49609e014137a5f955
Andrey Kumykov <aakumykov@yandex.ru>
2024-07-25 14:38:07 +0300

    Класс StorageDirectory теперь Parcelable; v3.0.0-alpha, versionCode 8

--- lib/src/main/java/com/github/aakumykov/android_storage_lister/StorageDirectory.kt
+++ lib/src/main/java/com/github/aakumykov/android_storage_lister/StorageDirectory.kt
@@ -1,7 +1,11 @@
 package com.github.aakumykov.android_storage_lister
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class StorageDirectory(
     override val type: AndroidStorageType,
     override val path: String,
     override val name: String,
-): AndroidStorageDirectory
+): AndroidStorageDirectory, Parcelable
